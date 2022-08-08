faab3f59508bcf94c0fbdadb19737ba8b74822da
megane42 <h.kzm.gm@gmail.com>
2022-08-08 15:39:09 +0900

    add aws.rb to summarize all requires

--- lib/ecs_autoscaling_scheduler.rb
+++ lib/ecs_autoscaling_scheduler.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "ecs_autoscaling_scheduler/version"
-require_relative "ecs_autoscaling_scheduler/aws/application_auto_scaling"
-require_relative "ecs_autoscaling_scheduler/aws/ecs"
+require_relative "ecs_autoscaling_scheduler/aws"
 
 module EcsAutoscalingScheduler
   class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ lib/ecs_autoscaling_scheduler/aws.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "aws/application_auto_scaling"
+require_relative "aws/ecs"
+
+module EcsAutoscalingScheduler
+  module Aws
+  end
+end
