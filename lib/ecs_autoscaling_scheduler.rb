fd7a2cc5c4c99cfb9d54ed23d320404914d2896e
megane42 <h.kzm.gm@gmail.com>
2022-08-08 12:14:33 +0900

    initial commit

---
+++ lib/ecs_autoscaling_scheduler.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ecs_autoscaling_scheduler/version"
+
+module EcsAutoscalingScheduler
+  class Error < StandardError; end
+  # Your code goes here...
+end
